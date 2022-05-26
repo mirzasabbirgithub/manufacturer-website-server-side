a06e66904d15085a66cdd4eafaa19b924c2fa0bc
mirzasabbirgithub <mirzaasabbir@gmail.com>
2022-05-26 16:24:59 +0600

    heroku issue fixing

--- index.js
+++ index.js
@@ -8,7 +8,21 @@ const port = process.env.PORT || 5000;
 
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
-app.use(cors());
+// app.use(cors());
+const corsConfig = {
+          origin: '*',
+          credentials: true,
+          methods: ['GET', 'POST', 'PUT', 'DELETE']
+}
+app.use(cors(corsConfig))
+app.options("*", cors(corsConfig))
+
+app.use(function (req, res, next) {
+          res.header("Access-Control-Allow-Origin", "*")
+          res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,authorization")
+          next()
+})
+
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.n949o.mongodb.net/?retryWrites=true&w=majority`;
